cdf64f01d01833adc1068b8f03c79da1dbf90228
unknown <sebbbes2@gmail.com>
2022-07-08 16:03:01 +0200

    testing

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Test from './Test';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <React.StrictMode><BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />}>
+        <Route path='test' element={<Test />} />
+      </Route>
+    </Routes>
+  </BrowserRouter></React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
