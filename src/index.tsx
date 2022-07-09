e19d55f641dce5fe4f2a642995ba4c4ac21e96e8
unknown <sebbbes2@gmail.com>
2022-07-09 16:05:55 +0200

    testing

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 import Test from './Test';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode><BrowserRouter>
-    <Routes>
-      <Route path='wave-function-collapse/' element={<App />} />
-      <Route path='wave-function-collapse/test' element={<Test />} />
-    </Routes>
-  </BrowserRouter></React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(
+
+  <HashRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='test' element={<Test />} />
+    </Routes>
+  </HashRouter>,
+  rootElement
+);
+// root.render(
+//   <React.StrictMode></React.StrictMode>
+// );
